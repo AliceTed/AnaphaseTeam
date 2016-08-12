c9563bca2bf86073cd536b55f6372c0dca608fb3
shibutooo <bega0409q@gmail.com>
2016-08-12 16:36:09 +0900

    アクタークラスのdraw関数にRenderer

--- AnaphaseTeam/AnaphaseTeam/header/actor/Actor.h
+++ AnaphaseTeam/AnaphaseTeam/header/actor/Actor.h
@@ -5,12 +5,13 @@
 *@date 2016/08/11
 */
 #pragma once
+class Renderer;
 class Actor
 {
 public:
 	virtual void initialize() = 0;
 	virtual void update(float deltatime) = 0;
-	virtual void draw() = 0;
+	virtual void draw(const Renderer &_renderer) = 0;
 	virtual void finish() = 0;
 	virtual void stand() = 0;
 	virtual void move() = 0;
