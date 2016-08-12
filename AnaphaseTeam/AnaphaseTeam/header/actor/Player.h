9772af236fb96d934961a33c1ffef028e19dd255
ひさあき <hisa-trude@outlook.jp>
2016-08-12 15:44:05 +0900

    playerクラス

--- AnaphaseTeam/AnaphaseTeam/header/actor/Player.h
+++ AnaphaseTeam/AnaphaseTeam/header/actor/Player.h
@@ -1,5 +1,24 @@
 #pragma once
-class Player
+/**
+*@file Player.h
+*@drief プレイヤークラス
+*@author 久秋雅
+*@date 2016/08/12
+*/
+#include "Actor.h"
+class Player :public Actor
 {
+public:
+	Player();
+	~Player();
+	void initialize();
+	void update();
+	void draw();
+	void finish();
+	void stand();
+	void move();
+	void attack();
+	void damage();
+private:
 
 };
---
+++ AnaphaseTeam/AnaphaseTeam/source/actor/Player.cpp
@@ -0,0 +1,37 @@
+#include "../../header/actor/Player.h"
+Player::Player()
+{}
+Player::~Player()
+{}
+void Player::initialize()
+{
+
+}
+void Player::update()
+{
+
+}
+void Player::draw()
+{
+
+}
+void Player::finish()
+{
+
+}
+void Player::stand()
+{
+
+}
+void Player::move()
+{
+
+}
+void Player::attack()
+{
+
+}
+void Player::damage()
+{
+
+}
