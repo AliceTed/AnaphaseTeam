5579d7176dee9f3af73995738db985d33db26c05
Rur1 <iikaaus@gmail.com>
2016-10-03 15:23:10 +0900

    仮

--- AnaphaseTeam/AnaphaseTeam/header/animation/AnimatorOne.h
+++ AnaphaseTeam/AnaphaseTeam/header/animation/AnimatorOne.h
@@ -7,10 +7,13 @@
 * @date 2016/08/16
 * @date 2016/8/30　isEndCurrentAnimationとisEndAnimation追加、コメント記入
 */
+#include<gslib.h>
 #include "../data/Model_ID.h"
 #include <map>
 #include <memory>
 #include "Animation.h"
+#include <vector>
+#include "../data/CastID.h"
 typedef std::shared_ptr<Animation> Animation_Ptr;
 class AnimatorOne
 {
@@ -21,7 +24,24 @@ public:
 	void initialize();
 	void update(float deltatime);
 	void bind()const;
-	void changeAnimation(ANIMATION_ID _animation, bool _isLoop=false, bool _isNotInit=false, float _animationSpeed = 1.0f);
+	template<class T>
+	void changeAnimation(T _animation, bool _isLoop = false, bool _isNotInit = false, float _animationSpeed = 1.0f)
+	{
+		Data::CastID cast;
+		if (m_currentAnimation == nullptr)
+			m_currentAnimation = std::make_shared<Animation>(m_modelID, cast(_animation), AnimationTimer(gsGetEndAnimationTime(cast(m_modelID), cast(_animation)), _animationSpeed), _isLoop);
+		///*今のアニメーションと同じなら変えない
+		if (m_currentAnimation->getAnimationNo() == static_cast<unsigned int>(_animation) && _animationSpeed == m_currentAnimation->getSpeed())
+			return;
+
+		m_currentAnimation = std::make_shared<Animation>(m_modelID, cast(_animation), AnimationTimer(gsGetEndAnimationTime(cast(m_modelID), cast(_animation)), _animationSpeed), _isLoop);
+		///*物によっては切り替え時に初期化しない
+		if (_isNotInit)
+			return;
+		m_currentAnimation->initialize();
+	}
+	//	void changeAnimation(ANIMATION_ID _animation, bool _isLoop = false, bool _isNotInit = false, float _animationSpeed = 1.0f);
+
 	/**
 	* @fn
 	* @breif 今動いているアニメーションが終わっているか
@@ -34,8 +54,13 @@ public:
 	* @detail 指定したものが動作中のアニメーションと違ければTrueを返す
 	*/
 	bool isEndAnimation(ANIMATION_ID _animationID);
+	void matrixCalculate(GSmatrix4* _reslut);
+	const GSuint getNumBones()const;
+	const GSmatrix4 getMatrix() const;
 private:
 	const MODEL_ID m_modelID;
 	ANIMATION_ID m_currentAnimationID;
 	Animation_Ptr m_currentAnimation;
+	std::vector<GSmatrix4> m_matrix;
+	GSmatrix4 m_mat;
 };
