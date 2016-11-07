64ebadadce47e710d4d207cb79f5b8df37518818
ひさあき <hisa-trude@outlook.jp>
2016-11-07 15:37:59 +0900

    コンボつなげた

--- AnaphaseTeam/AnaphaseTeam/source/actor/player/Player.cpp
+++ AnaphaseTeam/AnaphaseTeam/source/actor/player/Player.cpp
@@ -81,7 +81,7 @@ void Player::stand(float deltaTime)
 	}
 	moveMotionChange();
 	subActionStart();
-	m_animatorOne.changeAnimation(ANIMATION_ID::STAND, true, true);
+	m_animatorOne.changeAnimation(ANIMATION_ID::STAND,false, true, true);
 	//m_animatorOne.lerpBegin(ANIMATION_ID::STAND, false, true);
 	control();
 }
@@ -120,7 +120,7 @@ void Player::jump(float deltaTime)
 
 void Player::avoid(float deltaTime)
 {
-	m_animatorOne.changeAnimation(ANIMATION_ID::AVOID);
+	m_animatorOne.changeAnimation(ANIMATION_ID::AVOID,false);
 	//m_animatorOne.lerpBegin(ANIMATION_ID::AVOID, true);
 	m_SubAction.update(deltaTime, SubActionType::AVOID);
 	m_SubAction.jumpPowerOff();
@@ -213,7 +213,7 @@ void Player::moving(float deltaTime, bool isAnimation)
 	}
 	movement(deltaTime, speed);
 	if (!isAnimation)return;
-	m_animatorOne.changeAnimation(ANIMATION_ID::RUN, true, true, time);
+	m_animatorOne.changeAnimation(ANIMATION_ID::RUN,false, true, true, time);
 }
 
 
