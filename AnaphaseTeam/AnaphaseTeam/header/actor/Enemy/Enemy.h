b626713b4acde4db345e3a9190f84b5ee34950bb
AliceTed <week270@yahoo.co.jp>
2016-11-11 13:06:47 +0900

    Enemychange

--- AnaphaseTeam/AnaphaseTeam/header/actor/Enemy/Enemy.h
+++ AnaphaseTeam/AnaphaseTeam/header/actor/Enemy/Enemy.h
@@ -1,27 +1,34 @@
 #pragma once
-#include "../../../header/actor/ICharacter.h"
-#include "../../../header/actor/Actor.h"
+#include "../Actor.h"
+#include "../../collision/CollisionManager.h"
+class EnemyCollision;
+class Player;
+//éÊÇËÇ†Ç¶Ç∏enumÇ≈èÛë‘ï™ÇØ
+enum class STATE
+{
+	SPAWN,
+	STAND,
+	MOVE,
+	ATTACK,
+	DAMAGE
+};
 
-class Enemy : public Actor,public ICharacter
+class Enemy :public Actor
 {
 public:
-	Enemy();
-	~Enemy();
+	Enemy(const Transform& _transform);
+	~Enemy() = default;
+	void addCollisionGroup(CollisionManager*  _manager)override;
+	void initialize() override;
+	void update(float deltatime)override;
+	void draw(const Renderer& _renderer, const Camera& _camera)override;
 public:
-	//Actoråpè≥
-	void initialize();
-	void update(float deltatime);
-	void draw(const Renderer& _renderer, const Camera& _camera);
-	void finish();
-	
-public:
-	//ICharacter
-	void stand(float deltaTime);
-	void move(float deltaTime);
-	void attack(float deltaTime);
-	void damage(float deltaTime);
-	void jump(float deltaTime);
-	void avoid(float deltaTime);
-	void createColision();
-
+	void collisionChase(EnemyCollision* _collision);
+	void damage(Player* _player);
+private:
+	void state();
+	const bool isDamageState()const;
+private:
+	Group_Ptr m_group;
+	STATE m_state;
 };
--- AnaphaseTeam/AnaphaseTeam/source/actor/Enemy/Enemy.cpp
+++ AnaphaseTeam/AnaphaseTeam/source/actor/Enemy/Enemy.cpp
@@ -1,66 +1,96 @@
 #include "../../../header/actor/Enemy/Enemy.h"
 #include "../../../header/renderer/Renderer.h"
-
-Enemy::Enemy()
-	:Actor(
-		Transform({ 10,0.5f,-30 }, GSquaternion(0,0,0,0)),
-		MODEL_ID::PLAYER,
-		Sphere(GSvector3(0, 0, 0), 0),
-		Actor_Tag::PLAYER
-		)
+#include "../../../header/collision/EnemyCollision.h"
+#include "../../../header/math/Random.h"
+Enemy::Enemy(const Transform & _transform)
+	:Actor(_transform, MODEL_ID::ENEMY,
+		Sphere(GSvector3(0, 0, 0), 1.0f),
+		Actor_Tag::ENEMY),
+	m_group(std::make_shared<CollisionGroup>(this)),
+	m_state(STATE::SPAWN)
 {
-
 }
-
-Enemy::~Enemy()
+void Enemy::addCollisionGroup(CollisionManager * _manager)
 {
-
+	_manager->add(m_group);
 }
-
 void Enemy::initialize()
 {
-	Actor::initialize();
-	m_animatorOne.initialize();
-	m_animatorOne.changeAnimation(ANIMATION_ID::STAND, true, true);
+	Collision_Ptr actor = std::make_shared<EnemyCollision>(this);
+	m_group->add(actor);
+	//m_animator.start(Animation(static_cast<GSuint>(ENEMY_ANIMATION::SPAWN)));
+	m_state = STATE::SPAWN;
 }
-
 void Enemy::update(float deltatime)
 {
+	m_animatorOne.update(deltatime);
+	state();
+	sphereChases(GSvector3(0, 1, 0));
 }
 
 void Enemy::draw(const Renderer & _renderer, const Camera & _camera)
 {
-	_renderer.getDraw3D().drawMesh_calcu(MODEL_ID::PLAYER, m_transform, m_animatorOne, m_Color);
+	//‹——£‚Ì“§‰ß‚È‚Ç‚Íshader‚É”C‚¹‚é—\’è
+	FALSE_RETURN(isInsideView(_camera));
+	alphaBlend(_camera);
+
+	_renderer.getDraw3D().drawMesh_calcu(MODEL_ID::ENEMY, m_transform, m_animatorOne, m_Color);
 }
 
-void Enemy::finish()
+void Enemy::collisionChase(EnemyCollision * _collision)
 {
+	_collision->chase(m_transform.m_translate);
 }
 
-void Enemy::stand(float deltaTime)
+void Enemy::damage(Player * _player)
 {
+	if (!isDamageState())return;
+	m_state = STATE::DAMAGE;
+	//m_animator.start(Animation(static_cast<GSuint>(ENEMY_ANIMATION::DAMAGE)));
+	m_transform.translate_front(-0.1f);
 }
 
-void Enemy::move(float deltaTime)
+void Enemy::state()
 {
-}
-
-void Enemy::attack(float deltaTime)
+	Math::Random rand;
+	switch (m_state)
 	{
-}
-
-void Enemy::damage(float deltaTime)
-	{
-}
-
-void Enemy::jump(float deltaTime)
+	case STATE::SPAWN:
+		/*if (m_animator.isEnd())
 		{
-	}
+			m_state = STATE::STAND;
+			m_animator.start(Animation(static_cast<GSuint>(ENEMY_ANIMATION::IDEL), true));
+		}*/
+		break;
+	case STATE::STAND:
 
-void Enemy::avoid(float deltaTime)
-	{
+		/*if (rand(0, 120) == 0)
+		{
+			m_state = STATE::MOVE;
+			m_animator.start(Animation(static_cast<GSuint>(ENEMY_ANIMATION::WALK), true));
+		}*/
+		break;
+	case STATE::MOVE:
+		/*if (rand(0, 120) == 0)
+		{
+			m_state = STATE::STAND;
+			m_animator.start(Animation(static_cast<GSuint>(ENEMY_ANIMATION::IDEL), true));
+		}*/
+		//m_transform.translate_front(1.0f);
+		break;
+	case STATE::ATTACK:
+		break;
+	case STATE::DAMAGE:
+		/*if (m_animator.isEnd())
+		{
+			m_state = STATE::STAND;
+			m_animator.start(Animation(static_cast<GSuint>(ENEMY_ANIMATION::IDEL), true));
+		}*/
+		break;
+	}
 }
 
-void Enemy::createColision()
-	{
+const bool Enemy::isDamageState() const
+{
+	return m_state == STATE::STAND || m_state == STATE::MOVE || m_state == STATE::ATTACK;
 }
