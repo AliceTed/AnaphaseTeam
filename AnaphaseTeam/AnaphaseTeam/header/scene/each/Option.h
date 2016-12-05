cee08a1c5183a77c674f09eab8d05dac3587d27f
ひさあき <hisa-trude@outlook.jp>
2016-12-05 15:50:21 +0900

    Option

--- AnaphaseTeam/AnaphaseTeam/header/scene/each/Option.h
+++ AnaphaseTeam/AnaphaseTeam/header/scene/each/Option.h
@@ -11,6 +11,7 @@ public:
 	~Option();
 	void initialize();
 	void update(float deltaTime);
+	void slideStart(unsigned int _index);
 	void draw(IRenderer * renderer);
 	void finish();
 	const SceneMode next()const;
@@ -19,4 +20,6 @@ public:
 private:
 	SceneChange m_change;
 	std::vector<SlideImage> m_image;
+	float m_timer;
+	bool m_slideStart[2];
 };
--- AnaphaseTeam/AnaphaseTeam/source/scene/each/Option.cpp
+++ AnaphaseTeam/AnaphaseTeam/source/scene/each/Option.cpp
@@ -1,4 +1,5 @@
 #include "../../../header/scene/each/Option.h"
+#include "../../../header/renderer/define/SpriteRenderDesc.h"
 #include "../../../header/device/GameDevice.h"
 #include "../../../header/renderer/IRenderer.h"
 #include <algorithm>
@@ -7,6 +8,7 @@ Option::Option()
 	m_change(),
 	m_image()
 {
+	m_slideStart[0] = m_slideStart[1] = false;
 }
 
 Option::~Option()
@@ -18,24 +20,35 @@ void Option::initialize()
 	m_change.initialize();
 	m_change.begin();
 	m_image.clear();
-	SlideImage image(TEXTURE_ID::PAD_A, GSvector2(-300, 100), GSvector2(100, 100));
+	SlideImage image(TEXTURE_ID::OPTION, GSvector2(-300, 100), GSvector2(100, 100));
 	m_image.emplace_back(image);
-	SlideImage image2(TEXTURE_ID::PAD_B, GSvector2(100, 100), GSvector2(500, 100));
+	SlideImage image2(TEXTURE_ID::EXIT, GSvector2(-300, 300), GSvector2(100, 300));
 	m_image.emplace_back(image2);
+	m_timer = 0;
+	m_slideStart[0] = m_slideStart[1] = false;
 }
 
 void Option::update(float deltaTime)
 {
 	if (m_change.update(deltaTime))return;
-	
-	if (GameDevice::getInstacnce().input()->right())
+	m_timer += deltaTime * 1;
+	if (m_timer > 1)
+	{
+		slideStart(0);
+	}
+	if (m_timer > 20)
+	{
+		slideStart(1);
+	}
+
+	/*if (GameDevice::getInstacnce().input()->right())
 	{
 		std::for_each(m_image.begin(), m_image.end(), [](SlideImage& _image) {_image.slide();});
 	}
 	if (GameDevice::getInstacnce().input()->left())
 	{
 		std::for_each(m_image.begin(), m_image.end(), [](SlideImage& _image) {_image.slide(1,MODE::REVERSE);});
-	}
+	}*/
 	std::for_each(m_image.begin(), m_image.end(), [deltaTime](SlideImage& _image) {_image.update(deltaTime);});
 	if (GameDevice::getInstacnce().input()->jump())
 	{
@@ -43,10 +56,23 @@ void Option::update(float deltaTime)
 	}
 }
 
+void Option::slideStart(unsigned int _index)
+{
+	if (!m_slideStart[_index])
+	{
+		m_image[_index].slide(0.6f);
+		m_slideStart[_index] = true;
+	}
+}
+
+
 void Option::draw(IRenderer * renderer)
 {
-	//renderer.getDraw2D().textrue(TEXTURE_ID::OPTION_BACKGROUND, &GSvector2(0, 0));
-	//std::for_each(m_image.begin(), m_image.end(), [&renderer](SlideImage& _image) {_image.draw(renderer);});
+	SpriteRenderDesc backdesc;
+	backdesc.textureID =static_cast<GSuint>(TEXTURE_ID::OPTION_BACKGROUND);
+	renderer->render(backdesc);
+
+	std::for_each(m_image.begin(), m_image.end(), [&renderer](SlideImage& _image) {_image.draw(renderer);});
 }
 
 void Option::finish()
