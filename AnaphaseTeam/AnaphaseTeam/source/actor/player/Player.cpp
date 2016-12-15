d5769092f6d053fe823584ea13c4c9aeb1ebebc4
SaiL522 <youhoty@gmail.com>
2016-12-15 16:20:04 +0900

    コミット準備

--- AnaphaseTeam/AnaphaseTeam/source/actor/player/Player.cpp
+++ AnaphaseTeam/AnaphaseTeam/source/actor/player/Player.cpp
@@ -236,16 +236,16 @@ void Player::look_at(CameraController * _camera, GSvector3 * _target)
 	m_camera->lookAt_cameraTarget_player(position);
 	m_camera->lookAt_cameraTarget_enemy(*_target);
 
-	//_camera->change_cameraWork(E_CameraWorkID::TEST);
+	_camera->change_cameraWork(E_CameraWorkID::TEST);
 
-	if (m_isLockOn)
+	/*if (m_isLockOn)
 	{
 		_camera->change_cameraWork(E_CameraWorkID::LOCK_ON);
 	}
 	else
 	{
 		_camera->change_cameraWork(E_CameraWorkID::NORMAL);
-	}
+	}*/
 }
 void Player::createStates()
 {
