c4f4e0149af4d4fefeb245ef420510e2d14535a7
shibutooo <bega0409q@gmail.com>
2017-01-07 00:31:57 +0900

    スローモーション修正

--- AnaphaseTeam/AnaphaseTeam/source/state/player/HomingStartState.cpp
+++ AnaphaseTeam/AnaphaseTeam/source/state/player/HomingStartState.cpp
@@ -13,13 +13,9 @@ void Player::HomingStartState::start()
 void Player::HomingStartState::action(float deltaTime)
 {
 	Sleep(60);
-	if (GameDevice::getInstacnce().input()->avoid())
-	{
-		changeState(ACTOR_STATE::HOMING);
-	}
 	if (m_actor->m_animatorOne.isEndCurrentAnimation())
 	{
-		changeState(ACTOR_STATE::STAND);
+		changeState(ACTOR_STATE::HOMING);
 	}
 }
 Player::HomingStartState* Player::HomingStartState::clone() const
