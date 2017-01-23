ae679fbe269c4e105abd8d79d7e68081552d45b2
ひさあき <hisa-trude@outlook.jp>
2017-01-23 15:21:36 +0900

    MidRangeAI変更

--- AnaphaseTeam/AnaphaseTeam/source/state/enemy/MiddleRangeAI.cpp
+++ AnaphaseTeam/AnaphaseTeam/source/state/enemy/MiddleRangeAI.cpp
@@ -19,15 +19,15 @@ void MiddleRangeAI::think(Player * _player)
 	//*****************‹——£i’†j**********************************
 	//*****************‚¿‚å‚Á‚Æ‰“‚¢‚Æ‚±‚ë**********************
 	Math::Random rnd;
-	int act = rnd(0, 10);
-	if (act % 10 == 0)
+	int act = rnd(0, 3);
+	if (act == 0)
 	{
 		if (m_actor->requestDistance(EAI::ATTACKRANGE))
 			return;
 		m_actor->changeState(ACTOR_STATE::EMOVE);
 		return;
 	}
-	if (act % 2 == 1)
+	if (act == 1)
 	{
 		m_actor->changeState(ACTOR_STATE::ESLIDE);
 		return;
