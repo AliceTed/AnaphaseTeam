0e088081b6ac71645c1bd7b763f7c372ecb27d2a
SaiL522 <youhoty@gmail.com>
2017-01-31 18:17:56 +0900

    メニューのパーティクルエフェクトとりあえず完了

--- AnaphaseTeam/AnaphaseTeam/header/function/ParticleManager.cpp
+++ AnaphaseTeam/AnaphaseTeam/header/function/ParticleManager.cpp
@@ -3,11 +3,12 @@
 #include "function\Particle.h"
 #include "function\ParticleData.h"
 #include "convenient\Timer.h"
+#include <random>
 
 
 ParticleManager::ParticleManager() :
 	m_particleData(std::make_shared<ParticleData>()),
-	m_timer(std::make_shared<Timer>(10.f))
+	m_timer(std::make_shared<Timer>(2.f))
 {
 
 }
@@ -24,9 +25,13 @@ void ParticleManager::update(float _deltaTime)
 {
 	m_timer->update(_deltaTime);
 
+	//タイマーがなくなったらパーティクルを生成
 	if (m_timer->isEnd()) {
 		m_timer->initialize();
-		m_particleData->add(Particle(GSvector2(640, 720)));
+		std::random_device rnd;
+		std::mt19937 mt(rnd());
+		std::uniform_real_distribution<> rand(0, 1230);
+		m_particleData->add(Particle(GSvector2(rand(mt), 720)));
 	}
 
 	m_particleData->update(_deltaTime);
